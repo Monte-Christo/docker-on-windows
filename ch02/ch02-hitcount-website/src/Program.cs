e6379d8abbdfeffbf7eba4d49c85b1f36ffc2cf5
MonteChristo <edgar.r.knapp@gmail.com>
2020-11-23 18:05:17 +0100

    adapt for .Net 5.0

--- ch02/ch02-hitcount-website/src/Program.cs
+++ ch02/ch02-hitcount-website/src/Program.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace HitCountWebApp
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
-        }
+      {
+        CreateHostBuilder(args).Build().Run();
+      }
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+          Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
